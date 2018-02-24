1367bc1848210d81cc68b36bd3fc226cfedaaa0a
Ayaka Neko <neko@ayaka.moe>
2018-02-24 19:15:10 +0800

    Revert to Javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+// Local Files
+let Bot = require('./bot').Bot;
+let botctl = require('./bot').botctl;
+let botlog = require('./bot').Log;
+let config = require('./config');
+botctl.start();
+botlog.info("可以使用了哦w");
+Bot.startPolling();
