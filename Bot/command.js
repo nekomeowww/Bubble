2b1b2249fe0f45a93b616a79ae7dd484d0097ab3
Ayaka Neko <neko@ayaka.moe>
2018-03-09 23:32:48 +0800

    Added the info command as /info

--- Bot/command.js
+++ Bot/command.js
@@ -1,6 +1,10 @@
+// Local Files
+
 let bot = require('../bot');
 let config = require('../config');
-let msgctl = require('./message')
+let msgctl = require('./message');
+let ssnctl = require('./session');
+let packageInfo = require('../package.json');
 
 let Bot = bot.Bot;
 
@@ -11,6 +15,8 @@ let core = () => {
     bot.Bot.command(command.register('/meow'), (ctx) => command.meow(ctx));
     bot.Bot.command(command.register('/session'), (ctx) => msgctl.control.core(ctx))
     bot.Bot.command(command.register('/exit'), (ctx) => msgctl.control.core(ctx));
+    bot.Bot.command(command.register('/getSessionId'), (ctx) => ssnctl.getSessionId(ctx));
+    bot.Bot.command(command.register('/info'), (ctx => command.info(ctx)))
 }
 
 let command = {
@@ -50,6 +56,37 @@ let command = {
         }
         else {
             ctx.reply("无权使用。")
+        }
+    },
+    info(ctx) {
+        let Version = bot.botUsername + " 版本: " + packageInfo.version;
+        let messageId = "Message ID: " + ctx.message.message_id;
+        let messageType = "Message Type: " + ctx.message.chat.type;
+        let senderId = "Sender ID: " + ctx.message.from.id;
+
+        let chatTitle = "Chat Title: " + ctx.message.chat.title;
+        let chatId = "Chat ID: " +  ctx.message.chat.id;
+
+        let happyChat = "希望你开心喔！";
+        let happyGroup = "希望你喜欢" + config.nickname + "喔！";
+
+        ctx.reply(Version);
+        
+        bot.Log.debug(">>> INFO -" + ctx.message.date + "- Report");
+        bot.Log.debug(messageId);
+        bot.Log.debug(messageType);
+        bot.Log.debug(senderId);
+
+        if(ctx.message.chat.type == 'private') {
+            ctx.reply(happyChat);
+            bot.Log.debug(">>> INFO Report END <<<");
+        }
+        else {
+            ctx.reply(happyGroup);
+
+            bot.Log.debug(chatTitle);
+            bot.Log.debug(chatId);
+            bot.Log.debug(">>> INFO Report END <<<");
         }
     }
 };
