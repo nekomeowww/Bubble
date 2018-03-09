abd04595b8bb25ae40b7724acdb0142d0fbd6cb3
Ayaka Neko <neko@ayaka.moe>
2018-03-09 22:09:09 +0800

    Added the session mode so change the original code to session.js

--- Bot/message.js
+++ Bot/message.js
@@ -1,17 +1,41 @@
-let bot = require('../bot');
-let cmd = require('./command')
+// Local Files
 
-let send = (ctx, text) => { 
-    let data = cmd.command.commandCheck(ctx)
-    let array = data.split(' ');
-    let sendId = array[0];
-    array.shift()
-    let sendText ='';
-    for(let i of array) {
-        sendText += i
+let bot = require('../bot');
+let config = require('../config');
+let cmdctl = require('./command')
+let ssnctl = require('./session');
+
+let sessionId;
+
+let control = {
+    core(ctx) {
+        ssnctl.core(ctx);
     }
-    bot.TelegramClient.sendMessage(sendId, sendText);
-    bot.Log.trace("发送消息内容：" + sendText + "至 [ " + senderId + "] ");
 }
 
-module.exports = send;
+let forward = (ctx) => {
+    if(ctx.message.from.id == config.ownerId) {
+        return;
+    }
+    else {
+        if(ctx.message.from.username != undefined){
+            bot.TelegramClient.sendMessage(config.ownerId, "来自: @" + ctx.message.from.username + " [ " + ctx.message.from.id + " ]")
+        }
+        else if(ctx.message.from.first_name != undefined && ctx.message.from.last_name != undefined) {
+            bot.TelegramClient.sendMessage(config.ownerId, "来自: " + ctx.message.from.first_name + " " + ctx.message.from.last_name + " [ " + ctx.message.from.id + " ]")            
+        }
+        else if(ctx.message.from.first_name != undefined) {
+            bot.TelegramClient.sendMessage(config.ownerId, "来自: " + ctx.message.from.first_name + " [ " + ctx.message.from.id + " ]")
+        }
+        else if(ctx.message.from.last_name != undefined) {
+            bot.TelegramClient.sendMessage(config.ownerId, "来自: " + ctx.message.from.last_name + " [ " + ctx.message.from.id + " ]")
+        }
+        else {
+            bot.TelegramClient.sendMessage(config.ownerId, "来自: [ " + ctx.message.from.id + " ] ");
+        }
+        bot.TelegramClient.forwardMessage(config.ownerId, ctx.message.from.id, ctx.message.message_id);
+    }
+}
+
+exports.forward = forward;
+exports.control = control;
